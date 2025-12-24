8348b7a408d292e470d5eed54ca665465f5ea935
AntonKonst-QA <antonsergeevichk@gmail.com>
2025-12-24 12:46:33 +0300

    Add files via upload

---
+++ Work_12/DebugTask10.java
@@ -0,0 +1,14 @@
+package Homework.Work_12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// Код должен удалять элементы списка, но выбрасывает ConcurrentModificationException.
+public class DebugTask10 {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(Arrays.asList("Alice", "Bob", "Charlie"));
+        names.removeIf(name -> name.startsWith("A"));
+
+    }
+}
---
+++ Work_12/DebugTask8.java
@@ -0,0 +1,13 @@
+package Homework.Work_12;
+// Код должен сравнить два числа, но почему-то результат не соответствует ожиданиям.
+public class DebugTask8 {
+    public static void main(String[] args) {
+        double a = 0.1 * 3;
+        double b = 0.3;
+        if (Math.abs(a - b) < 0.01) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
+}
